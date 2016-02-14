c40c960be472a2f3a5e1b9e31387bb0609344120
lsprung-tgm <lsprung@student.tgm.ac.at>
2016-02-14 23:57:12 +0100

    Testklasse 3 Unit tests

--- CSV_Test.py
+++ CSV_Test.py
@@ -1,10 +1,25 @@
-from Sprung_CSV import readCSV, writeinCSV, sniffer, newfile
 import unittest
+from csv_class import Sprung_CSV
+
 
 class TestMyFunctions(unittest.TestCase):
-    def read_file(self):
-        pfad = "C:\\Users\\lspru\\Desktop\\ergebnissems"
-        self.assertFalse(readCSV(pfad),[])
+    def read_file_test(self):
+        test = Sprung_CSV("C:\\Users\\lspru\\Desktop\\ergebnissems")
+        self.assertFalse(test.readCSV(),[])
+
+    def read_new_file_test(self):
+        test = Sprung_CSV("C:\\Users\\lspru\\Desktop\\ergebnissems")
+        test.newfile()
+        test2 = Sprung_CSV("C:\\Users\\lspru\\Desktop\\ergebnissemsNEW")
+        self.assertFalse(test2.readCSV(),[])
+
+    def read_new_file_test2(self):
+        test = Sprung_CSV("C:\\Users\\lspru\\Desktop\\ergebnissems")
+        test.writeinCSV()
+        test2 = Sprung_CSV("C:\\Users\\lspru\\Desktop\\ergebnissemsNEU")
+        self.assertFalse(test2.readCSV(),[])
 
 if __name__ == '__main__':
     unittest.main()
+
+
