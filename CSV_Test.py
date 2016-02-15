c77b31d414a46c86552dfdb817d7c4e84a8ef7c8
lsprung-tgm <lsprung@student.tgm.ac.at>
2016-02-15 17:16:30 +0100

    4 Tests

--- CSV_Test.py
+++ CSV_Test.py
@@ -9,6 +9,15 @@ class TestMyFunctions(unittest.TestCase):
     def testread(self):
         self.test.readCSV()
 
+    def testwrite(self):
+        self.test.writeinCSV()
+
+    def testwrite2(self):
+        self.test.newfile()
+
+    def testsniffer(self):
+        self.testsniffer()
+
 if __name__ == '__main__':
     unittest.main()
 
